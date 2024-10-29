22386ef9f251c84a37b2cee8971daf4828dc0174
Alex <zemzikova.oleksandra@gmail.com>
2024-10-29 21:53:12 +0200

    base

---
+++ src/components/Options/Options.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Options = (updateFeedback) => {
+  return (
+    <div>
+      <button onClick={updateFeedback}>Good</button>
+      <button onClick={updateFeedback}>Neutral</button>
+      <button onClick={updateFeedback}>Bad</button>
+    </div>
+  );
+};
+
+export default Options;
